2ec8927145c85f8e26eac91c9b84490d0464a8da
Curley <curley@utexas.edu>
2023-02-14 11:00:02 -0600

    add schizophrenia heatmap

---
+++ code/collecting_datasets.R
@@ -0,0 +1,11 @@
+### Where datasets came from - acknowledgements
+
+# Diseases
+diseases <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-12-10/diseases.csv")
+write.csv(diseases, "data_raw/diseases.csv", row.names=F)
+head(diseases)
+
+#Schizophrenia
+schz <- season::schz
+head(schz)
+write.csv(schz,"data_raw/schz.csv", row.names=F)
