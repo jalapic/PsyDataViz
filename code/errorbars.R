37d2b9a0d6712a33c7ca799e0389ff27f256ed46
jalapic <curley@utexas.edu>
2023-02-20 13:07:31 -0600

    significance levels more

---
+++ code/errorbars.R
@@ -0,0 +1,6 @@
+#errorbars
+
+https://semba-blog.netlify.app/03/25/2020/barplot-with-errorbar-in-ggplot2/
+  
+  http://www.sthda.com/english/wiki/ggplot2-error-bars-quick-start-guide-r-software-and-data-visualization
+
